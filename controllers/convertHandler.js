0102b9a5bdc5d8d6e13bfce7fedc12b515e546f1
Nassim Kitouni <naki363@gmail.com>
2025-05-20 22:33:24 +0100

    feat:completing convertHandler.getNum

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,77 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    let inputArr = input.split(/[A-z]+/);
+    if (inputArr.length === 1) {
+      return isNaN(inputArr[0]) || inputArr[0] === ""
+        ? "Invalid number"
+        : +inputArr[0];
+    }
+    if (inputArr.length === 2) {
+      if (!isNaN(inputArr[1]) && inputArr[1] !== "") {
+        return "Invalid number";
+      }
+      if (inputArr[0] === "") {
+        return 1;
+      }
+      if (inputArr[0].split("/").includes("")) {
+        return "Invalid number";
+      }
+      if (inputArr[0].split("/").length === 1) {
+        return +inputArr[0];
+      }
+      if (inputArr[0].split("/").length === 2) {
+        return +inputArr[0].split("/")[0] / +inputArr[0].split("/")[1];
+      }
+    }
+
+    return "Invalid number";
   };
-  
-  this.getUnit = function(input) {
+
+  this.getUnit = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
+// const test = new ConvertHandler();
+// console.log("2kg: " + test.getNum("2kg"));
+// console.log("2kg6: " + test.getNum("2kg6"));
+// console.log("3.5mi: " + test.getNum("3.5mi"));
+// console.log("3/5l: " + test.getNum("3/5l"));
+// console.log("3/5.5l: " + test.getNum("3/5.5l"));
+// console.log("3/5/7l: " + test.getNum("3/5/7l"));
+// console.log("mig5: " + test.getNum("mig5"));
+// console.log("mi: " + test.getNum("mi"));
+// console.log("/: " + test.getNum("/"));
+// console.log("5: " + test.getNum("5"));
+// console.log("/gh: " + test.getNum("/gh"));
+// console.log(": " + test.getNum(""));
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,56 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite("Unit Tests", function () {
+  test("Testing reading a whole number input.", () => {
+    assert.strictEqual(
+      convertHandler.getNum("2mi"),
+      2,
+      "convertHandler should correctly read a whole number input.",
+    );
+  });
 
+  test("Testing reading a decimal number input.", () => {
+    assert.strictEqual(
+      convertHandler.getNum("3.1kg"),
+      3.1,
+      "convertHandler should correctly read a decimal number input.",
+    );
+  });
+
+  test("Testing reading a fractional input.", () => {
+    assert.strictEqual(
+      convertHandler.getNum("3/5l"),
+      0.6,
+      "convertHandler should correctly read a fractional input.",
+    );
+  });
+
+  test("Testing reading a fractional input with a decimal.", () => {
+    assert.strictEqual(
+      convertHandler.getNum("1.8/2lbs"),
+      0.9,
+      "convertHandler should correctly read a fractional input with a decimal.",
+    );
+  });
+
+  test("Testing returning an error on a double-fraction", () => {
+    assert.strictEqual(
+      convertHandler.getNum("3/5/7kg"),
+      "Invalid number",
+      "convertHandler should correctly return an error on a double-fraction",
+    );
+  });
+
+  test("Testing defaulting to a numerical input of 1 when no numerical input is provided.", () => {
+    assert.strictEqual(
+      convertHandler.getNum("mi"),
+      1,
+      "convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.",
+    );
+  });
 });
+
